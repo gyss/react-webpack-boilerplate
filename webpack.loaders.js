eb2e1ead3a22482b7406708e58936da5b9464fcc
Kote <gravedian@gmail.com>
2016-08-11 08:43:40 +0100

    changed babel config to .babelrc

--- webpack.loaders.js
+++ webpack.loaders.js
@@ -1,11 +1,19 @@
 module.exports = [
     // .js or .jsx files => compile ES6 and JSX
-    {
+    /*{
         test: /.jsx?$/,
         loader: 'babel-loader',
         exclude: /node_modules/,
         query: {
             presets: ['es2015', 'react']
         }
-    }
+    }*/
+
+  
+
+    {
+		test: /\.jsx?$/,
+		exclude: /node_modules/,
+		loaders: ['react-hot', 'babel'],
+	}
 ];
